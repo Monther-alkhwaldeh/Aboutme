12196a5dc252c743a9440b13ce1bb92847036182
monther-alkhwaldeh <mmskbh.91@gmail.com>
2021-02-08 16:56:40 +0200

    add 201 lab 02

---
+++ js/js.js
@@ -0,0 +1,55 @@
+'use strict';
+let typeLaptop=prompt('it is hp your favorite laptop yes/no');
+let typeVga=prompt(' it is nividea your favorite vga card yes/no');
+let typeMouse=prompt('it is Microsoft your favorite mouse yes/no');
+let typeKeyboard=prompt('it is microsoft your favorite keyboard yes/no');
+let typeHeadset=prompt(' it is hyberx your favourite headset yes/no');
+
+if(typeLaptop.toLowerCase() === 'yes' ||typeLaptop.toLowerCase() === 'y' ){
+  alert('you hp is fav laptop');
+  console.log(typeLaptop);
+}else {
+  alert('you hp is not fav laptop');
+}
+if(typeVga.toLowerCase() === 'yes' ||typeVga.toLowerCase() === 'y' ){
+  alert('you hp is fav vga card');
+  console.log(typeVga);
+
+}else {
+  alert('you hp is not fav vga');
+}
+if(typeMouse.toLowerCase() === 'yes' ||typeMouse.toLowerCase() === 'y' ){
+  alert('you hp is fav Mouse');
+  console.log(typeMouse);
+
+}else {
+  alert('you hp is not fav Mouse');
+}
+if(typeKeyboard.toLowerCase() === 'yes' ||typeKeyboard.toLowerCase() === 'y' ){
+  alert('you hp is fav Keyboard');
+  console.log(typeKeyboard);
+
+}else {
+  alert('you hp is not fav Keyboard');
+}
+if(typeHeadset.toLowerCase() === 'yes' ||typeHeadset.toLowerCase() === 'y' ){
+  alert('you hp is fav headset');
+  console.log(typeHeadset);
+
+}else {
+  alert('you hp is not fav headset');
+}
+
+let userName=prompt('Enter your username');
+console.log(userName);
+alert('welcome to the website:' + '  ' + userName);
+alert('YOUR USERNAME IS : ' + '  ' +userName);
+
+
+
+
+
+
+
+
+
