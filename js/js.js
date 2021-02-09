9355493a15650ea2eed2f93d98a434a6df607d7c
monther-alkhwaldeh <mmskbh.91@gmail.com>
2021-02-09 17:59:53 +0200

    lab 03

--- js/js.js
+++ js/js.js
@@ -1,49 +1,124 @@
 'use strict';
-let typeLaptop=prompt('it is hp your favorite laptop yes/no');
-let typeVga=prompt(' it is nividea your favorite vga card yes/no');
-let typeMouse=prompt('it is Microsoft your favorite mouse yes/no');
-let typeKeyboard=prompt('it is microsoft your favorite keyboard yes/no');
-let typeHeadset=prompt(' it is hyberx your favourite headset yes/no');
+let userName=prompt('Enter your username');
+console.log(userName);
+alert('welcome to the website:' + '  ' + userName);
+alert('YOUR USERNAME IS : ' + '  ' +userName);
+let correctAns=0;
 
+let typeLaptop=prompt('it is hp your favorite laptop yes/no');
 if(typeLaptop.toLowerCase() === 'yes' ||typeLaptop.toLowerCase() === 'y' ){
   alert('you hp is fav laptop');
+  correctAns+=1;
   console.log(typeLaptop);
 }else {
   alert('you hp is not fav laptop');
 }
+let typeVga=prompt(' it is nividea your favorite vga card yes/no');
 if(typeVga.toLowerCase() === 'yes' ||typeVga.toLowerCase() === 'y' ){
   alert('you hp is fav vga card');
   console.log(typeVga);
+  correctAns+=1;
 
 }else {
   alert('you hp is not fav vga');
 }
+let typeMouse=prompt('it is Microsoft your favorite mouse yes/no');
 if(typeMouse.toLowerCase() === 'yes' ||typeMouse.toLowerCase() === 'y' ){
   alert('you hp is fav Mouse');
   console.log(typeMouse);
+  correctAns+=1;
 
 }else {
   alert('you hp is not fav Mouse');
 }
+let typeKeyboard=prompt('it is microsoft your favorite keyboard yes/no');
 if(typeKeyboard.toLowerCase() === 'yes' ||typeKeyboard.toLowerCase() === 'y' ){
   alert('you hp is fav Keyboard');
   console.log(typeKeyboard);
+  correctAns+=1;
 
 }else {
   alert('you hp is not fav Keyboard');
 }
+let typeHeadset=prompt(' it is hyberx your favourite headset yes/no');
+
 if(typeHeadset.toLowerCase() === 'yes' ||typeHeadset.toLowerCase() === 'y' ){
   alert('you hp is fav headset');
   console.log(typeHeadset);
+  correctAns+=1;
 
 }else {
   alert('you hp is not fav headset');
 }
 
-let userName=prompt('Enter your username');
-console.log(userName);
-alert('welcome to the website:' + '  ' + userName);
-alert('YOUR USERNAME IS : ' + '  ' +userName);
+let guessNumber=prompt('guess number ');
+let correctNumber=10;
+let tryn=4;
+
+while( tryn){
+  if(guessNumber === correctNumber.toString()){
+    alert('your answer is correct');
+    console.log(correctNumber);
+    correctAns+=1;
+    console.log(correctAns);
+    break;
+  }
+
+  else if(guessNumber < correctNumber){
+    alert('its is low');
+    console.log(guessNumber);
+  } else if (guessNumber > correctNumber) {
+    alert('its high');
+    console.log(guessNumber);
+  }
+  tryn-=1;
+  guessNumber=prompt('please try again');
+  
+  if(tryn===1){
+    alert('the correct answer is :' + correctNumber);
+    break;
+  }
+}
+
+
+let placesList=['goa','london','rome','petra','cairo','moscow','madrid','dubai','beirut','baghdad'];
+console.log(placesList);
+console.log(placesList.length);
+
+
+let attempt=6;
+let userGuess=prompt('please guess one of the top ten places' );
+let correct=false;
+while(attempt && !correct){
+  for (let i=0;i<placesList.length;i++){
+    if(userGuess === placesList[i]){
+      alert('your choice is coreect');
+      correct=true;
+      console.log(userGuess);
+      correctAns+=1;
+      console.log(correctAns);
+      alert('you got a ' + correctAns + ' correct answers');
+      break;
+    }
+    attempt-=1;
+    userGuess=prompt('please try again you have' + attempt + 'remaining');
+    console.log(correctAns);
+    break;
+  }
+  if (correct ){
+    break;
+  }
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
